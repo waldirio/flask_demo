03d222d5d90d65e996c56352a21a5fbfb961366c
Waldirio M Pinheiro <waldirio@gmail.com>
2025-08-04 18:03:54 -0700

    upload test

--- app.py
+++ app.py
@@ -1,7 +1,33 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import tempfile
+import shutil
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+UPLOAD_DIR = 'pictures'
+
+@app.route('/upload', methods=['POST','GET'])
+def upload_files():
+    if not os.path.exists(UPLOAD_DIR):
+        os.makedirs(UPLOAD_DIR)
+    if request.method == 'POST':
+        if 'files' not in request.files:
+            return 'No files part'
+        files = request.files.getlist('files')
+        if not files:
+            return 'No files selected'
+        if files:
+            temp_dir = tempfile.mkdtemp()
+            for file in files:
+                file.save(os.path.join(temp_dir, file.filename))
+            # Process the files here
+            # You can now save the files or send them back to the client
+            for file in files:
+                shutil.copy(os.path.join(temp_dir, file.filename), UPLOAD_DIR)
+            shutil.rmtree(temp_dir)
+            return 'Files uploaded successfully'
+    return render_template('upload.html')
+
+if __name__ == '__main__':
+    app.run()
